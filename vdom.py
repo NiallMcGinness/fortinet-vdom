385a3843686d77310ec175dbf49098c34b9645db
Niall McGinness <nmcginness@radiant.net>
2018-05-15 23:30:15 -0700

    add listAll function to get list of all vdoms configured

--- vdom.py
+++ vdom.py
@@ -8,7 +8,17 @@ class vdom(object):
         self.channel = channel
         self.modem_password = modem_password
         self.output = ""
-	
+    
+    def listAll(self):
+        self.channel.send('config global \r')
+        #exit_status = self.channel.recv_exit_status()
+        #print("exit_status {} ".format(exit_status))
+        time.sleep(2)
+        vdom_list = self.channel.send('get sys vdom-property \r')
+        time.sleep(2)
+        self.channel.send('end \r')
+        return vdom_list  
+
     def getWanIp(self):
         self.channel.send('conf vdom \r')
         time.sleep(2)
@@ -25,6 +35,6 @@ class vdom(object):
         else:
             ip = None
         return ip
-    
+
     def getOutput(self):
         return self.output
